4599188f77946e2196d29b663f44b8d6eb8a432f
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-01-09 02:02:08 -0500

    Practice using Hooks and rendering components according to a searchbar

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sit ipsa ullam asperiores a aperiam iste consectetur beatae consequatur inventore necessitatibus laboriosam delectus accusamus! Nulla optio minima, itaque aliquam, sint eos aspernatur ad ut quidem, quasi repellat est delectus non. Quis modi amet id ipsa incidunt eum culpa minima asperiores perferendis unde, cumque delectus illum enim quidem excepturi omnis velit.
+      </p>
+    </section>
+  )
+}
+
+export default About
